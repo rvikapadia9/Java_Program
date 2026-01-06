424f9e7ddfcf61ef1fd9d96c8bdb4915ca4bdc8b
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2026-01-06 18:25:25 +0530

    test

--- Java_Program/src/Ex_01.java
+++ Java_Program/src/Ex_01.java
@@ -1,2 +1,14 @@
-public class Ex_01 {
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+
+
+    }
 }
