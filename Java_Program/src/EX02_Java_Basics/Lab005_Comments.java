a2e46c8aa079a5d0b2a4bbf74bca2d0e92d2ad44
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2026-01-06 18:46:46 +0530

    test

---
+++ Java_Program/src/EX02_Java_Basics/Lab005_Comments.java
@@ -0,0 +1,17 @@
+package EX02_Java_Basics;
+
+public class Lab005_Comments {
+    public static void main(String[] args) {
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        System.out.println("Hello World!");
+
+
+    }
+}
---
+++ Java_Program/src/EX02_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,42 @@
+package EX02_Java_Basics;
+
+/**
+ * Author : Ravi Kapadia
+ * Course : Automation
+ * Learning :  Java
+ *
+ **/
+
+public class Lab006_Comments_Multi {
+    /**
+     * Author : Ravi kapadia
+     * Course :  Automation
+     * Learning :  Java
+     *
+     **/
+
+    // This is sample
+    public static void main(String[] args) {
+        /**
+         * Author : Ravi Kapadia
+         * Course :  Automation
+         * Learning :  Java
+         *
+         **/
+
+       /*
+
+        System.out.println("Heloo");
+        System.out.println("Heloo");
+
+        */
+
+
+    }
+}
+/**
+ * Author : Ravi Kapadia
+ * Course :  Automation
+ * Learning :  Java
+ *
+ **/
