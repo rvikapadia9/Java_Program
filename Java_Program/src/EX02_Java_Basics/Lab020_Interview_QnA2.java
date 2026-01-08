4ba574525a9c0217f24f6d2489e0efb3130e5503
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2026-01-08 14:43:16 +0530

    08-Jan

---
+++ Java_Program/src/EX02_Java_Basics/Lab020_Interview_QnA2.java
@@ -0,0 +1,12 @@
+package EX02_Java_Basics;
+public class Lab020_Interview_QnA2 {
+    public static void main(String[] args) {
+//        int enum = 10;
+        int Enum = 10;
+//        System.out.println(enum);
+        //        int ravi kapadia  = 89;
+        int ravi$kapadia = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+    }
+}
